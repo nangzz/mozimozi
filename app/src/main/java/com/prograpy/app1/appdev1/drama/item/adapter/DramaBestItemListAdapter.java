de9983f47be76c573f67aaa4d471cf669b981231
이승준 <6951004@naver.com>
2018-05-23 17:07:12 +0900

    랭크 관련 이미지 추가 / 팝업 클릭 리스너 오류 수정

--- app/src/main/java/com/prograpy/app1/appdev1/drama/item/adapter/DramaBestItemListAdapter.java
+++ app/src/main/java/com/prograpy/app1/appdev1/drama/item/adapter/DramaBestItemListAdapter.java
@@ -1,5 +1,6 @@
 package com.prograpy.app1.appdev1.drama.item.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,8 +15,12 @@ public class DramaBestItemListAdapter extends RecyclerView.Adapter<DramaBestItem
 
     private View.OnClickListener onClickListener;
 
+    private Context mContext;
+
     @Override
     public BestItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        mContext = parent.getContext();
 
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_drama_best_item_child, parent,false);
 
@@ -26,6 +31,7 @@ public class DramaBestItemListAdapter extends RecyclerView.Adapter<DramaBestItem
     public void onBindViewHolder(BestItemViewHolder holder, int position) {
 
         ((BestItemViewHolder)holder).setOnItemClick(onClickListener);
+        ((BestItemViewHolder)holder).setRankImage(position);
 
     }
 
@@ -42,8 +48,13 @@ public class DramaBestItemListAdapter extends RecyclerView.Adapter<DramaBestItem
 
     public class BestItemViewHolder extends RecyclerView.ViewHolder {
 
+        private int[] resRank = {R.drawable.img_best_n1, R.drawable.img_best_n2, R.drawable.img_best_n3, R.drawable.img_best_n4,
+                                R.drawable.img_best_n5, R.drawable.img_best_n6, R.drawable.img_best_n7, R.drawable.img_best_n8,
+                R.drawable.img_best_n9, R.drawable.img_best_n10};
+
         private LinearLayout parent;
         private ImageView itemImg;
+        private ImageView itemRankImg;
         private TextView itemPrice;
         private TextView itemName;
 
@@ -53,14 +64,18 @@ public class DramaBestItemListAdapter extends RecyclerView.Adapter<DramaBestItem
 
             parent = (LinearLayout) itemView.findViewById(R.id.view_parent);
             itemImg = (ImageView) itemView.findViewById(R.id.item_image);
+            itemRankImg = (ImageView) itemView.findViewById(R.id.item_rank_img);
             itemPrice = (TextView) itemView.findViewById(R.id.item_price);
             itemName = (TextView) itemView.findViewById(R.id.item_name);
-
-
         }
 
         public void setOnItemClick(View.OnClickListener listener){
             parent.setOnClickListener(listener);
+        }
+
+
+        public void setRankImage(int rank){
+            itemRankImg.setImageDrawable(mContext.getResources().getDrawable(resRank[rank]));
         }
     }
 
