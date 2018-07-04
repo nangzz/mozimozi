55724ad22df131758884ae1c313788c2548decf4
이승준 <6951004@naver.com>
2018-07-04 13:59:18 +0900

    Revert "Revert "검색관련 task 추가""
    This reverts commit 116d8da5ae224063031a424f93a077f7fadaf7f5.

---
+++ app/src/main/java/com/prograpy/app1/appdev1/task/SearchAsyncTask.java
@@ -0,0 +1,84 @@
+package com.prograpy.app1.appdev1.task;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.prograpy.app1.appdev1.network.HttpRequest;
+import com.prograpy.app1.appdev1.network.response.SearchResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Note on 2018-06-30.
+ */
+
+public class SearchAsyncTask extends AsyncTask<String, Integer, SearchResult> {
+
+private SearchHandler handler;
+
+
+public interface SearchHandler{
+    public void onSuccessAppAsyncTask(SearchResult result);
+    public void onFailAppAsysncask();
+    public void onCancelAppAsyncTask();
+}
+
+
+
+    public SearchAsyncTask(SearchAsyncTask.SearchHandler handler){
+        this.handler = handler;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected SearchResult doInBackground(String... strings) {
+
+        String path = strings[0];
+        String searchname = strings[1];
+
+
+        SearchResult result  = null;
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("searchname", searchname);
+
+        HttpRequest request = new HttpRequest();
+
+        try {
+            String str = request.callRequestServer(path,  "POST", params);
+
+            Log.d("http", "str > " + str);
+
+
+            Gson gson = new GsonBuilder().create();
+            result = gson.fromJson(str, SearchResult.class);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(SearchResult AppAsyncTaskResult) {
+        super.onPostExecute(AppAsyncTaskResult);
+
+        if(AppAsyncTaskResult != null){
+            handler.onSuccessAppAsyncTask(AppAsyncTaskResult);
+
+        }else{
+            handler.onFailAppAsysncask();
+        }
+
+    }
+}
