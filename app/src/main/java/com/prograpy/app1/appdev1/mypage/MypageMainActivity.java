14812b442a74d267ee0aeaa7901c32e18e9759ab
이승준 <6951004@naver.com>
2018-07-04 10:51:41 +0900

    마이페이지 서버 통신 예외 처리 수정

--- app/src/main/java/com/prograpy/app1/appdev1/mypage/MypageMainActivity.java
+++ app/src/main/java/com/prograpy/app1/appdev1/mypage/MypageMainActivity.java
@@ -13,9 +13,11 @@ import com.prograpy.app1.appdev1.R;
 
 import com.prograpy.app1.appdev1.network.ApiValue;
 import com.prograpy.app1.appdev1.network.response.CategoryResult;
+import com.prograpy.app1.appdev1.network.response.MyPageProductResult;
 import com.prograpy.app1.appdev1.popup.NetworkProgressDialog;
 import com.prograpy.app1.appdev1.popup.info.CustomPopup;
 import com.prograpy.app1.appdev1.task.MypageProductAsyncTask;
+import com.prograpy.app1.appdev1.utils.PerferenceData;
 import com.prograpy.app1.appdev1.view.TopbarView;
 
 
@@ -64,59 +66,27 @@ public class MypageMainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
 
 
-/*        List<MyPageItemData> myPageItemData = new ArrayList<>();
-        MyPageItemData[] myPageItemData_array = new MyPageItemData[7];
-        myPageItemData_array[0] = new MyPageItemData(R.drawable.my_golden_life_bag1, "52회 서은수 가방", "빈치스", "239,230원");
-        myPageItemData_array[1] = new MyPageItemData(R.drawable.my_golden_life_bag2, "33회 서은수 가방", "만다리나덕", "297,310원");
-        myPageItemData_array[2] = new MyPageItemData(R.drawable.my_golden_life_acc1, "52회 서은수 안경", "젠틀몬스터", "250,000원");
-        myPageItemData_array[3] = new MyPageItemData(R.drawable.my_golden_life_acc2, "52회 신혜선 귀걸이", "러브캣 비쥬", "142,560원");
-        myPageItemData_array[4] = new MyPageItemData(R.drawable.my_golden_life_shirt1, "52회 서은수 티셔츠", "코인코즈", "31,200원");
-        myPageItemData_array[5] = new MyPageItemData(R.drawable.my_golden_life_shoes1, "52회 신혜선 운동화", "슈콤마보니", "305,760원");
-        myPageItemData_array[6] = new MyPageItemData(R.drawable.my_golden_life_pants1, "37회 신혜선 청바지", "레이브", "89,180원");
-
-
-        for(int i = 0; i < 7; i++)
-            myPageItemData.add(myPageItemData_array[i]);
-
-        myPageListAdapter = new MyPageListAdapter(getApplicationContext(), myPageItemData, R.layout.activity_mypage_main);
-        recyclerView.setAdapter(myPageListAdapter);
-*/
-
         myPageListAdapter = new MyPageListAdapter(getApplicationContext(), itemPopupListener);
         recyclerView.setAdapter(myPageListAdapter);
 
         networkProgressDialog.show();
 
-        MypageProductAsyncTask mypageProductAsyncTask = new MypageProductAsyncTask(new MypageProductAsyncTask.CategoryResultHandler() {
+        MypageProductAsyncTask mypageProductAsyncTask = new MypageProductAsyncTask(new MypageProductAsyncTask.TaskResultHandler() {
             @Override
-            public void onSuccessAppAsyncTask(CategoryResult result) {
+            public void onSuccessAppAsyncTask(MyPageProductResult result) {
 
                 networkProgressDialog.dismiss();
 
-                if(result != null){
-                    Log.d("TAG", result.success + "\n" + result.mypageProductList );
+                Log.d("TAG", result.isSuccess() + "\n" + result.getMypageProductList());
 
-                    if(result.success){
+                if (result.isSuccess()) {
 
-                        if(result.mypageProductList != null && result.mypageProductList.size() > 0){
-                            myPageListAdapter.setMyPageItemData(result.mypageProductList);
-                            myPageListAdapter.notifyDataSetChanged();
-                        }
-                        else{
-                            Toast.makeText(MypageMainActivity.this, getResources().getString(R.string.failed_server_connect), Toast.LENGTH_SHORT).show();
-                        }
-                    }else{
-
-                        Toast.makeText(MypageMainActivity.this, getResources().getString(R.string.failed_server_connect), Toast.LENGTH_SHORT).show();
+                    if (result.getMypageProductList() != null && result.getMypageProductList().size() > 0) {
+                        myPageListAdapter.setMyPageItemData(result.getMypageProductList());
+                        myPageListAdapter.notifyDataSetChanged();
                     }
 
-                }else{
-
-                    Toast.makeText(MypageMainActivity.this, getResources().getString(R.string.failed_server_connect), Toast.LENGTH_SHORT).show();
                 }
-
-
-
             }
 
             @Override
@@ -137,7 +107,7 @@ public class MypageMainActivity extends AppCompatActivity {
             }
         });
 
-        mypageProductAsyncTask.execute(ApiValue.API_MYPAGE, getIntent().getStringExtra("type"));
+        mypageProductAsyncTask.execute(ApiValue.API_MYPAGE, PerferenceData.getKeyUserId());
 
     }
 
