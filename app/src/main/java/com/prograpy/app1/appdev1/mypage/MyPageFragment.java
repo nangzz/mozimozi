9063e8898a1a03b0d97c77064242a67f36b77395
이승준 <6951004@naver.com>
2018-07-19 18:28:32 +0900

    마이페이지랑 찜리스트 교체

--- app/src/main/java/com/prograpy/app1/appdev1/mypage/MyPageFragment.java
+++ app/src/main/java/com/prograpy/app1/appdev1/mypage/MyPageFragment.java
@@ -24,18 +24,6 @@ import com.prograpy.app1.appdev1.vo.ProductVO;
 
 public class MyPageFragment extends Fragment {
 
-    private MyPageListAdapter myPageListAdapter;
-    private RecyclerView recyclerView;
-
-    private View.OnClickListener itemActivityListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            CustomPopup infoPopup = new CustomPopup(getContext());
-            infoPopup.show();
-        }
-    };
-
-
     public static MyPageFragment createFragment(){
 
         Bundle bundle = new Bundle();
@@ -44,68 +32,6 @@ public class MyPageFragment extends Fragment {
         fragment.setArguments(bundle);
 
         return fragment;
-    }
-
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_mypage_main, container, false);
-
-        recyclerView = (RecyclerView) view.findViewById(R.id.item_list_mypage);
-
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
-        recyclerView.setLayoutManager(gridLayoutManager);
-        recyclerView.setHasFixedSize(true);
-
-        myPageListAdapter = new MyPageListAdapter(getContext(), itemActivityListener);
-        recyclerView.setAdapter(myPageListAdapter);
-
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        MypageProductAsyncTask mypageProductAsyncTask = new MypageProductAsyncTask(new MypageProductAsyncTask.TaskResultHandler() {
-            @Override
-            public void onSuccessAppAsyncTask(MyPageProductResult result) {
-
-
-                if (result.isSuccess()) {
-
-                    if (result.getMypageProductList() != null && result.getMypageProductList().size() > 0) {
-                        myPageListAdapter.setMyPageItemData(result.getMypageProductList());
-                        myPageListAdapter.notifyDataSetChanged();
-
-
-                        DbController.deleteAll(getContext());
-
-                        for(ProductVO item : result.getMypageProductList()){
-                            DbController.addProductId(getContext(), item.getP_id());
-                        }
-                    }
-
-                }
-            }
-
-            @Override
-            public void onFailAppAsysncask() {
-                Toast.makeText(getContext(), getResources().getString(R.string.failed_server_connect), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCancelAppAsyncTask() {
-
-                Toast.makeText(getContext(), getResources().getString(R.string.failed_server_connect), Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        mypageProductAsyncTask.execute(ApiValue.API_MYPAGE, PreferenceData.getKeyUserId());
-
     }
 
 }
